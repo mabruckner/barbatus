840da04f180e219baba048d837ac8ca477651a35
analogbit <mbruckner807@gmail.com>
2014-01-02 13:33:43 -0600

    tictac8

--- tictactoe.py
+++ tictactoe.py
@@ -1,37 +1,37 @@
 #import game
 
 class TicTacToe :
-    def __init__(self):
+    def __init__(self) :
         self.board=[[None for j in range(3)] for i in range(3)]
         self.players=[]
 
-    def addPlayer(self,player):
+    def add_player(self,player) :
         self.players += [player]
 
-    def begin(self):
+    def begin(self) :
         if len(self.players) is not 0:
-            self.currentplayer = 0
+            self.current_player = 0
 
-    def score(self):
+    def score(self) :
         out = {}
         for player in self.players :
-            out[player]=self.scoreboard(player)
+            out[player]=self.score_board(player)
         return out
 
-    def scoreboard(self,player):
+    def score_board(self,player) :
         num = 0
-        for x in range(3):
-            for y in range(3):
-                num += self.scorecell(player,(x,y))
+        for x in range(3) :
+            for y in range(3) :
+                num += self.score_cell(player,(x,y))
         return num
 
-    def score_cell(self,player,pos):
+    def score_cell(self,player,pos) :
         num = 0
-        for dx in range( 2 if pos[0] == 0 else 1):
-            for dy in range( 2 if pos[1] == 0 else 1):
+        for dx in range( 2 if pos[0] == 0 else 1) :
+            for dy in range( 2 if pos[1] == 0 else 1) :
                 if dx !=0 and dy!=0 :
                     num += 1
-                    for n in range(3):
+                    for n in range(3) :
                         if self.board[pos[0]+n*dx][pos[1]+n*dy] != player :
                             num -= 1
                             break
@@ -46,17 +46,17 @@ class TicTacToe :
                 return True
         return False
 
-    def validate(self,player,move):
+    def validate(self,player,move) :
         if player not in self.players :
             return False
-        if self.players.index(player)!=self.currentplayer :
+        if self.players.index(player)!=self.current_player :
             return False
         if not isinstance(move,dict):
             return False
         if "x" not in move or "y" not in move :
             return False
         indexrange = range(3)
-        if move["x"] not in indexrange or move["y"] not in indexrange :
+        if move["x"] not in indexrange or move["y"] not in index_range :
             return False
         if self.board[move["x"]][move["y"]] is not None :
             return False
@@ -64,7 +64,7 @@ class TicTacToe :
 
     def execute(self,player,move):
         self.board[move["x"]][move["y"]] = player
-        self.currentplayer = (self.currentplayer+1)%len(self.players)
+        self.currentplayer = (self.current_player+1) % len(self.players)
         return True
 
     def get_state(self,player):
