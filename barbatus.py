dde84d740fcf86174cf0b6de5c3a2ee92f5e101e
analogbit <mbruckner807@gmail.com>
2014-01-02 19:47:49 -0600

    stuff

--- barbatus.py
+++ barbatus.py
@@ -1,2 +1,17 @@
+import tornado.ioloop
 import tornado.web
 
+class StaticHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(self.request.uri[1:])
+
+class TemplateHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates"+self.request.uri)
+
+application = tornado.web.Application([
+    (r"/static/.*",StaticHandler),
+    (r"/.*",TemplateHandler)],debug=True)
+
+application.listen(8888)
+tornado.ioloop.IOLoop.instance().start()
